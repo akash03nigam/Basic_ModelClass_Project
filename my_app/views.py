295f07d9aab2f5c379e8d18d150d1054aca82b5b
akash03nigam <40694250+akash03nigam@users.noreply.github.com>
2021-06-02 09:58:14 +0530

    Using Datatbase ORM

--- my_app/views.py
+++ my_app/views.py
@@ -3,9 +3,10 @@ from my_app.models import Employee
 from my_app.models import Student
 # Create your views here.
 def employee_info(request):
-    employ= Employee.objects.all()
+    employ= Employee.objects.order_by('Escore') #Using Database ORM(Object Relational Mapping)
     return render(request, 'test_app/results.html', {'e': employ})
 
 def student_info(request):
     stu= Student.objects.all()
+    #stu= Student.objects.filter(marks_lt=35)
     return render(request, 'test_app/results2.html',{'s':stu})   
